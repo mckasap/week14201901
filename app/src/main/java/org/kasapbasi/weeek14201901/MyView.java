cca6204b1a7a8a5dacf030d882e89ac02de8b985
mckasap <mckasap@gmail.com>
2019-05-10 14:48:41 +0300

    U can touch this

--- app/src/main/java/org/kasapbasi/weeek14201901/MyView.java
+++ app/src/main/java/org/kasapbasi/weeek14201901/MyView.java
@@ -1,4 +1,46 @@
 package org.kasapbasi.weeek14201901;
 
-public class MyView {
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MyView extends View {
+    /**
+     * Simple constructor to use when creating a view from code.
+     *
+     * @param context The Context the view is running in, through which it can
+     *                access the current theme, resources, etc.
+     */
+    Context c;
+
+    public MyView(Context context) {
+        super(context);
+        this.c=context;
+    }
+
+    /**
+     * Pass the touch screen motion event down to the target view, or this
+     * view if it is the target.
+     *
+     * @param event The motion event to be dispatched.
+     * @return True if the event was handled by the view, false otherwise.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                Toast.makeText(c,  event.getY() +" Tıklandı " +event.getX(), Toast.LENGTH_SHORT).show();break;
+
+
+             }
+
+
+
+        return  true;
+    }
 }
